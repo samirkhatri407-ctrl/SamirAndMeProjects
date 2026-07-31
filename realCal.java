f92941b9e71169bdd6d948d8851067ce86686283
Samir Khatri <samirkhatri407@gmail.com>
2026-07-31 20:35:50 +0900

    calculator created

---
+++ realCal.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+public class realCal{
+    public static void main(String[] args){
+        //system.in le Scanner lai kata bata chai input kata bata liyera use garni hoo
+        Scanner scanner = new Scanner(System.in);
+         double num1;
+         double num2;
+         char operator;
+         double result=0;
+         boolean validOperation = true;
+
+         System.out.print("Enter the first number:");
+         num1 = scanner.nextDouble();
+
+         System.out.print("Enter an operator(+,-,*,/):");
+         operator = scanner.next().charAt(0);
+
+         System.out.print("Enter an operator(+,-,*,/):");
+         num2 = scanner.nextDouble();
+
+         switch(operator){
+            case'+' -> result = num1 + num2;
+            case'-' -> result = num1 - num2;
+            case'*' -> result = num1 * num2;
+            case'/' ->{
+                if(num2== 0){
+                    System.out.println("Cannot divide by zero!");
+                    validOperation = false;
+                }
+                else{
+                    result = num1 / num2;
+                }
+            }
+            default -> {
+                System.out.println("Invalid operator!");
+                validOperation = false;
+            }
+
+         }
+         if(validOperation){
+        System.out.println(result);
+
+         }
+
+
+
+        scanner.close();
+    }
+}
